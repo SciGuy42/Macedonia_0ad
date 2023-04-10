03bb6939d01cc332bea21f760ecdf6a73cd82d8b
LangLangbart <92653266+LangLangBart@users.noreply.github.com>
2023-04-10 14:21:15 +0200

    fix: remove redundant map settings from scenario maps
    added a map filter Keyword "macedonian_campaign"

---
+++ gui/maps/MapFilters_Macedonia_0ad.js
@@ -0,0 +1,7 @@
+MapFilters.prototype.Filters.unshift(
+	{
+		"Name": "Macedonian Campaign",
+		"Title": translate("Macedonian Campaign"),
+		"Description": translate("Campaign created by SciGuy42"),
+		"Match": ["macedonian_campaign"]
+	});
